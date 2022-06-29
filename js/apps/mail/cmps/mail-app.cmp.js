94c7879559861e3268c294e3c00fc105019d0396
hadar gueta <hadart20@gmail.com>
2022-06-29 21:18:47 +0300

    ח

---
+++ js/apps/mail/cmps/mail-app.cmp.js
@@ -0,0 +1,54 @@
+import mailList from "../../mail/cmps/mail-list.cmp.js"
+import mailCompose from "./mail-compose.cmp.js"
+// import mailDetails from "./mail-details.cmp.js"
+import { mailService } from "../services/mail-service.js"
+export default {
+    template: ` 
+    <section class="main-list">
+        <h2>Hello</h2>
+        <!-- <button @click="newMail">+</button> -->
+        <!-- <mail-compose v-if="this.newMail"/> -->
+        <mail-list />
+    </section>
+   `,
+    data() {
+        return {
+            mails: null,
+            selectedMail: null,
+            newMail: false
+        };
+    },
+    created() {
+        console.log("here")
+        this.mails = mailService.query().then(res => this.mails = res)
+
+    },
+    mounted() {
+    },
+    methods: {
+        showEmails(email) {
+            this.selectedMail = email
+        },
+        newMail() {
+            this.newMail = true;
+        }
+
+
+    },
+    computed: {
+        renderMails() {
+            console.log(this.mails)
+            return this.mails
+        }
+    },
+    unmounted() { },
+    components: {
+        mailList,
+        // mailDetails: mailDetails
+        mailCompose
+    },
+    destroyed() { }
+
+};
+
+
